131e37a6b02f7ca231714b97fe9e7fc57c66b5de
Wes Lanstr <wesley.lancaster.dev@gmail.com>
2026-04-16 10:53:42 -0400

    added button changing text

--- DoodlePromtGenerator/Pages/Index.cshtml.cs
+++ DoodlePromtGenerator/Pages/Index.cshtml.cs
@@ -1,4 +1,5 @@
 using DoodlePromptGenerator;
+using Humanizer;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -6,23 +7,49 @@ namespace DoodlePromptGenerator.Pages
 {
     public class IndexModel : PageModel
     {
-        private readonly PromptBuilder _promptBuilder;
-
-        public IndexModel()
-        {
-            _promptBuilder = new PromptBuilder();
-        }
+        private readonly PromptBuilder _promptBuilder = new();
 
         [BindProperty]
         public string Output { get; set; } = "";
 
+        [BindProperty]
+        public int PromptCount { get; set; }
+
         public void OnGet()
         {
+            PromptCount = 0;
         }
 
         public void OnPostGenerate()
         {
+            PromptCount++;
             Output = PromptFormatter.BuildPromptWithAscii(_promptBuilder);
+        }
+
+        public string GetButtonText()
+        {
+            if (PromptCount == 0)
+                return "Give me an art prompt!";
+
+            return $"Give me an {(PromptCount + 1).ToOrdinalWords()} art prompt!";
+        }
+
+        private string ToOrdinalWords(int number)
+        {
+            return number switch
+            {
+                1 => "First",
+                2 => "Second",
+                3 => "Third",
+                4 => "Fourth",
+                5 => "Fifth",
+                6 => "Sixth",
+                7 => "Seventh",
+                8 => "Eighth",
+                9 => "Ninth",
+                10 => "Tenth",
+                _ => $"{number}th"
+            };
         }
     }
 }
